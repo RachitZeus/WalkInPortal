3a4c7ad711febeb0001d1894aa3db9592ccc713b
RachitZeus <rachit.modi@zeuslearning.com>
2024-02-06 15:56:42 +0530

    nextcommit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginModule } from './login/login.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
